7fa38a8f095cde0acff32e261b98c6b7a70b86df
German Alvaredo <geralvaredo@github.com>
2020-10-28 17:15:06 -0300

    registro para pacientes y profesionales sin subida de foto

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -5,6 +5,7 @@ import {Perfil} from '../../clases/perfil';
 import {PerfilService} from '../../servicios/perfil.service';
 import {Paciente} from '../../clases/paciente';
 import {Profesional} from '../../clases/profesional';
+import {Especialidad} from '../../clases/especialidad';
 
 @Component({
   selector: 'app-registro',
@@ -13,25 +14,28 @@ import {Profesional} from '../../clases/profesional';
 })
 export class RegistroComponent implements OnInit {
 
-  isRegistered = 'bienvenido';
+  isRegistered = 'verificacionCorreo';
   registerError = 'error';
   repass: string ;
   usuarioError: boolean;
   passError: boolean;
   repassError: boolean;
+  nombreError: boolean;
   apellidoError: boolean;
   fechaDeNacimientoError: boolean;
   primeraFotoError: boolean;
   segundaFotoError: boolean;
   especialidadError: boolean;
+  captchaError: boolean;
   sexoError: boolean;
   tipoError: boolean;
+  error: boolean ;
   usuario: Usuario;
-  perfil: Perfil;
+  perfil: any ;
   tipoPerfil: string;
   documento: string;
   token: string ;
-  captchaError: boolean;
+
   especialidad: string[] =
     ['Cardiologia', 'Radiologia', 'Traumatologia', 'Oftalmologia' , 'Neurologia' , 'Alergista' , 'Enfermeria'];
   seleccionados: string [] = [];
@@ -41,8 +45,9 @@ export class RegistroComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.usuario = new Usuario();
     this.restablecer();
+    this.usuario = new Usuario();
+    this.inicializar();
   }
 
   onChange(e): void{
@@ -55,8 +60,14 @@ export class RegistroComponent implements OnInit {
 
   async onRegister(): Promise<void> {
     try {
-      const user = await this.auth.register(this.usuario);
-      this.registroDireccionamiento(user);
+       if (this.validarCampos()){
+         const user = await this.auth.register(this.usuario);
+         this.perfil.uid  = user.uid;
+         this.auth.sitioAnterior('registro');
+         this.auth.guardarEnStorage(user);
+         this.registroDireccionamiento(user);
+         this.restablecer();
+       }
     } catch (error) {
       console.log('ErrorOnRegister', error);
     }
@@ -66,6 +77,9 @@ export class RegistroComponent implements OnInit {
     if (user) {
       this.verificacion();
       this.perfil.id = this.documento;
+      if (this.tipoPerfil === 'Profesional'){
+         this.cargaEspecialidad();
+       }
       this.pr.crearPerfil(this.perfil);
       this.auth.redirect(this.isRegistered);
     } else {
@@ -76,13 +90,18 @@ export class RegistroComponent implements OnInit {
   verificacion(): void {
     if (this.tipoPerfil === 'Paciente'){
        this.perfil.tipo = 'Paciente';
-       this.isRegistered = 'verificacionCorreo';
     }
     else{
       this.perfil.tipo = 'Profesional';
     }
   }
 
+  cargaEspecialidad(): void {
+    for (let i = 0; i < this.seleccionados.length; i++) {
+      this.perfil.especialidades.push(new Especialidad(i, this.seleccionados[i]));
+    }
+
+  }
 
   asignarId(): void{
     this.pr.contadorPerfiles().subscribe((lista) => {
@@ -90,13 +109,143 @@ export class RegistroComponent implements OnInit {
     });
   }
 
-  restablecer(): void{
+  inicializar(): void{
     this.asignarId();
+  }
+
+  restablecer(): void {
+    this.usuarioError = false;
+    this.passError = false;
+    this.repassError = false;
+    this.nombreError = false;
+    this.apellidoError = false;
+    this.fechaDeNacimientoError = false;
+    this.primeraFotoError = false;
+    this.segundaFotoError = false;
+    this.especialidadError = false;
+    this.sexoError = false;
+    this.captchaError = false;
   }
 
   resolved(token): void{
     this.token = token;
   }
 
+   validarCampos(): boolean {
+    this.error = false;
+    let fotos = true;
+    let especialidad = true;
+    if (this.tipoPerfil === 'Paciente'){
+         fotos = this.validarFotos();
+      }
+    if (this.tipoPerfil === 'Profesional'){
+         especialidad = this.validarEspecialidad();
+      }
+    const usuario = this.validarUsuario();
+    const pass = this.validarPass();
+    const nombre = this.validarNombre();
+    const apellido = this.validarApellido();
+    const fecha = this.validarFecha();
+    const sexo = this.validarSexo();
+    const captcha = this.validarCaptcha();
+
+    if (usuario && pass && nombre && apellido && fecha && sexo && fotos && especialidad && captcha){
+        this.error = true;
+       }
+    return this.error;
+   }
+
+   validarUsuario(): boolean {
+      let usuario = true ;
+      if (this.usuario.email === null || this.usuario.email === undefined){
+       this.usuarioError = true;
+       usuario = false;
+     }
+
+      return usuario;
+   }
+
+   validarPass(): boolean{
+     let pass = true ;
+     let repass = true;
+     if (this.usuario.pass === null || this.usuario.pass === undefined){
+       this.passError = true;
+       pass = false;
+     }
+     if (this.repass === null || this.repass === undefined){
+        this.repassError = true;
+        repass = false;
+     }
+
+     return (pass && repass);
+   }
+
+   validarNombre(): boolean{
+      let nombre = true;
+      if (this.perfil.nombre === null || this.perfil.nombre === undefined){
+       this.nombreError = true;
+       nombre = false;
+     }
+      return nombre;
+   }
+
+  validarApellido(): boolean {
+    let apellido = true;
+    if (this.perfil.apellido === null || this.perfil.apellido === undefined){
+      this.apellidoError = true;
+      apellido = false;
+    }
+    return apellido;
+  }
+
+  validarFecha(): boolean {
+    let fecha = true;
+    if (this.perfil.fechaNacimiento === null || this.perfil.fechaNacimiento === undefined){
+      this.fechaDeNacimientoError = true;
+      fecha = false;
+    }
+    return fecha;
+  }
+
+  validarSexo(): boolean {
+    let sexo = true;
+    if (this.perfil.sexo === null || this.perfil.sexo === undefined){
+      this.sexoError = true;
+      sexo = false;
+    }
+    return sexo;
+  }
+
+  validarFotos(): boolean {
+     let primera = true ;
+     let segunda = true ;
+     if (this.perfil.img1 === null || this.perfil.img1 === undefined){
+       this.primeraFotoError = true ;
+       primera = false;
+    }
+     if (this.perfil.img2 === null || this.perfil.img2 === undefined){
+      this.segundaFotoError = true;
+      segunda = false;
+    }
+     return (primera && segunda);
+  }
+
+  validarEspecialidad(): boolean {
+    let especialidad = true;
+    if (this.seleccionados === null || this.seleccionados === undefined){
+      this.especialidadError = true;
+      especialidad = false;
+    }
+    return especialidad;
+  }
+
+  validarCaptcha(): boolean {
+      let tk = true;
+      if (this.token === null || this.token === undefined){
+       this.captchaError = true;
+       tk = false;
+    }
+      return tk;
+  }
 
 }
