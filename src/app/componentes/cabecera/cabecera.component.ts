78d4c376965339d6ae7d8be7b8e9f8038a13a0f3
German Alvaredo <geralvaredo@github.com>
2020-10-29 03:43:00 -0300

    foto de perfil en la cabecera

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Usuario} from '../../clases/usuario';
 import {AuthService} from '../../servicios/auth.service';
+import {ImagenService} from '../../servicios/imagen.service';
+import {PerfilService} from '../../servicios/perfil.service';
 
 @Component({
   selector: 'app-cabecera',
@@ -9,11 +11,42 @@ import {AuthService} from '../../servicios/auth.service';
 })
 export class CabeceraComponent implements OnInit {
 
-  usuario: Usuario;
+  usuario: string;
+  img: string ;
+  nombre: string;
+  perfil: string ;
 
-  constructor(public auth: AuthService ) { }
+  constructor(public auth: AuthService , private foto: ImagenService , private pr: PerfilService ) { }
 
   ngOnInit(): void {
+    this.traerUsuario();
+
   }
+
+  fotoPerfil(): void {
+    console.log(this.img);
+    this.foto.getUpload('imagenes/' + this.img ).then(
+      res => { this.perfil = res; }
+    );
+  }
+
+ traerUsuario(): void{
+     this.usuario = JSON.parse(this.auth.getStorage('usuario')).uid;
+     this.pr.contadorPerfiles().subscribe(
+      (lista: any) => {
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < lista.length; i++) {
+           if (this.usuario === lista[i].uid){
+             this.img = lista[i].img1;
+             this.nombre = lista[i].nombre;
+             this.fotoPerfil();
+           }
+
+        }
+      }
+    );
+
+ }
+
 
 }
