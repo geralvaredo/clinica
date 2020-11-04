a2f25d6c407c09271c23efa73c923bff58315fd7
German Alvaredo <geralvaredo@github.com>
2020-11-04 16:50:54 -0300

    clase administrador, servicio de turnos y adaptador fecha

---
+++ src/app/clases/administrador.ts
@@ -0,0 +1,15 @@
+import {Pais} from './pais';
+
+export class Administrador {
+  uid: string;
+  id: string;
+  nombre: string;
+  apellido: string;
+  tipo: string;
+  fechaNacimiento: string;
+  nacionalidad: Pais;
+  sexo: string ;
+  fechaAlta: string;
+  fechaBaja: string;
+  img1: string;
+}
---
+++ src/app/servicios/turno.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {FirestoreService} from './firestore.service';
+import {Turno} from '../clases/turno';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TurnoService {
+
+  turno: Turno;
+
+  constructor(private fs: FirestoreService) { }
+
+  public ultimoTurnoId(): Observable<any> {
+    return this.fs.obtenerTotalDocumentos('turnos');
+  }
+
+  public agregarListaDeTurnos(listaTurno: Array<Turno> ): void {
+    for (let i = 0; i < listaTurno.length; i++) {
+      this.turno = new Turno(listaTurno[i].id.toString(), listaTurno[i].fecha , listaTurno[i].estado ,
+        listaTurno[i].horaInicio, listaTurno[i].minutoInicio, listaTurno[i].horaFin, listaTurno[i].minutoFin,
+        listaTurno[i].profesional, listaTurno[i].paciente , listaTurno[i].especialidad);
+      this.fs.agregarDocumento(JSON.parse( JSON.stringify(this.turno)), 'turnos');
+    }
+  }
+
+  public modificarTurno(turno : Turno){
+    this.fs.modificarDocumento(turno, 'turnos');
+  }
+
+}
