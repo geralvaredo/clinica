8420cc70d33c3771270ba1b02cb5cd0e58b1d4d9
German Alvaredo <geralvaredo@github.com>
2020-11-05 17:28:09 -0300

    Alta de administradores

---
+++ src/app/componentes/alta-admin/alta-admin.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-admin',
+  templateUrl: './alta-admin.component.html',
+  styleUrls: ['./alta-admin.component.scss']
+})
+export class AltaAdminComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
