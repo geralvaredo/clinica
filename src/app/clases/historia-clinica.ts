b3c99469f44371671e16e702709d3434fc2ca1a7
German Alvaredo <geralvaredo@github.com>
2020-11-05 17:36:06 -0300

    cambios en entidad historia clinica

--- src/app/clases/historia-clinica.ts
+++ src/app/clases/historia-clinica.ts
@@ -2,10 +2,12 @@ import {Paciente} from './paciente';
 import {Profesional} from './profesional';
 
 export class HistoriaClinica {
+  paciente: Paciente;
+  profesional: Profesional;
   resenaPaciente: string;
   resenaProfesional: string;
   edad: string;
-  temperaturaCorporal: string;
+  temperatura: string;
   presion: string;
   primeraObservacion: string;
   segundaObservacion: string;
